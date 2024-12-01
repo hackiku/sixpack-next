4f9205ec071ec4995c0db89b49fd0940880f2a7e
hackiku <ivan@pipewriter.io>
2024-12-02 00:45:54 +0100

    html layout overflow-x-clip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en" className={`${GeistSans.variable}`}>
+		<html lang="en" className={`${GeistSans.variable} overflow-x-clip`}>
 			<body>
 				<Navbar />
 				<TRPCReactProvider>
